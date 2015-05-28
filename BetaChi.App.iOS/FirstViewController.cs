0dee9c47ef9bbef211c44cf7e3f8850b9a65f9f1
Lucas Wyland <lucaswyland11@gmail.com>
2015-05-28 18:00:49 -0500

    Added Projects

---
+++ BetaChi.App.iOS/FirstViewController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+using Foundation;
+using UIKit;
+
+namespace BetaChi.App.iOS
+{
+    public partial class FirstViewController : UIViewController
+    {
+        public FirstViewController(IntPtr handle)
+            : base(handle)
+        {
+            Title = NSBundle.MainBundle.LocalizedString("First", "First");
+            TabBarItem.Image = UIImage.FromBundle("Images/first");
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        #region View lifecycle
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+        }
+
+        #endregion
+    }
+}
---
+++ BetaChi.Core.iOS/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BetaChi.Core.iOS
+{
+    public class Class1
+    {
+    }
+}
