743ab8d74c988b5176c1da9c880380308af2843f
Lucas Wyland <lucaswyland11@gmail.com>
2015-05-31 08:51:57 -0500

    Added Files Not Included

---
+++ BetaChi.Core.Android/Day.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BetaChi.Core.Android
+{
+    public class Day
+    {
+        public string Id { get; set; }
+        public DateTime CurrentDate { get; set; }
+
+        public MealItem Meals { get; set; }
+
+        public ICollection<ReminderItem> Reminders { get; set; }
+        public ICollection<DriverItem> SoberDrivers { get; set; }
+    }
+}
---
+++ BetaChi.Core.Android/IM.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BetaChi.Core.Android
+{
+    class IM
+    {
+        public string Id { get; set; }
+        public DateTime CurrentDate { get; set; }
+
+        public ICollection<ReminderItem> Reminders { get; set; }
+
+        public ICollection<TeamItem> Leaderboard { get; set; }
+
+        // TODO: Possibly Add Brotherhood Points
+    }
+}
