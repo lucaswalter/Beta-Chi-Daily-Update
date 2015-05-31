537d877654eef69763306e8003ca227b53b4810b
Lucas Wyland <lucaswyland11@gmail.com>
2015-05-30 21:04:46 -0500

    Early Model Draft

--- BetaChi.Core.Android/EventItem.cs
+++ BetaChi.Core.Android/EventItem.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
 namespace BetaChi.Core.Android
 {
-    public class Class1
+    class EventItem
     {
+        public string Id { get; set; }
+        public string Text { get; set; }
     }
 }
