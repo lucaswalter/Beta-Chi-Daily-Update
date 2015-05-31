3de4307aa9e10eff16fb1b626b0bd87bd395cc32
Lucas Wyland <lucaswyland11@gmail.com>
2015-05-31 08:48:43 -0500

    IM Data Model Draft

---
+++ BetaChi.Core.Android/TeamItem.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BetaChi.Core.Android
+{
+    class TeamItem
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        public int Points { get; set; }
+    }
+}
