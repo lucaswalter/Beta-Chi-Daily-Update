1de62881f885b8bc8d7db5bc0392696f7eabe847
Lucas Wyland <lucaswyland11@gmail.com>
2015-05-31 12:55:44 -0500

    Complete Change Of Project Structure

---
+++ AndroidApp/Core/DriverItem.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace AndroidApp.Core
+{
+    public class DriverItem
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        public DateTime ShiftStart { get; set; }
+        public DateTime ShiftEnd { get; set; }
+    }
+}
---
+++ AndroidApp/Screens/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidApp.Screens
+{
+    [Activity(Label = "Beta-Chi Daily Update", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set View From Main Layout Resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
