6e2fc7b17d070c0b0501f1798123a3b98c322414
Lucas Wyland <lucaswyland11@gmail.com>
2015-06-03 23:51:42 -0500

    Added Toolbar Layouts

--- AndroidApp/Resources/Resource.Designer.cs
+++ AndroidApp/Resources/Resource.Designer.cs
@@ -66,29 +66,32 @@ namespace AndroidApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060004
-			public const int breakfastTextView = 2131099652;
-			
-			// aapt resource value: 0x7f060000
-			public const int dateTextView = 2131099648;
-			
-			// aapt resource value: 0x7f060006
-			public const int dinnerTextView = 2131099654;
+			// aapt resource value: 0x7f060005
+			public const int breakfastTextView = 2131099653;
 			
 			// aapt resource value: 0x7f060001
-			public const int loadingProgressBar = 2131099649;
-			
-			// aapt resource value: 0x7f060005
-			public const int lunchTextView = 2131099653;
-			
-			// aapt resource value: 0x7f060003
-			public const int mealTextView = 2131099651;
-			
-			// aapt resource value: 0x7f060002
-			public const int reminderListView = 2131099650;
+			public const int dateTextView = 2131099649;
 			
 			// aapt resource value: 0x7f060007
-			public const int soberDriverButton = 2131099655;
+			public const int dinnerTextView = 2131099655;
+			
+			// aapt resource value: 0x7f060002
+			public const int loadingProgressBar = 2131099650;
+			
+			// aapt resource value: 0x7f060006
+			public const int lunchTextView = 2131099654;
+			
+			// aapt resource value: 0x7f060004
+			public const int mealTextView = 2131099652;
+			
+			// aapt resource value: 0x7f060003
+			public const int reminderListView = 2131099651;
+			
+			// aapt resource value: 0x7f060008
+			public const int soberDriverButton = 2131099656;
+			
+			// aapt resource value: 0x7f060000
+			public const int toolbar = 2131099648;
 			
 			static Id()
 			{
@@ -105,6 +108,9 @@ namespace AndroidApp
 			
 			// aapt resource value: 0x7f030000
 			public const int MainActivity = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Toolbar = 2130903041;
 			
 			static Layout()
 			{
