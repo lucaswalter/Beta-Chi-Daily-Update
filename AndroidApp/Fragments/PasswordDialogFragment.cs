1251f809dd37e9ffe02851fe4863ec0e083c9645
Lucas Wyland <lucaswyland11@gmail.com>
2015-06-08 21:52:09 -0500

    Layout Tweaks

--- AndroidApp/Fragments/PasswordDialogFragment.cs
+++ AndroidApp/Fragments/PasswordDialogFragment.cs
@@ -94,7 +94,6 @@ namespace AndroidApp.Fragments
                     break;
                 }
             }
-
         }
 
         private void HandleNegativeButtonClick(object sender, DialogClickEventArgs e)
