05f7bb1c82f1a1709fff0e4197cc889dc1811dbb
Lucas Wyland <lwyland@swankmp.com>
2015-06-19 10:33:01 -0500

    Removed Unused Models And Cleaned Up Current

--- AndroidApp/Core/ReminderItem.cs
+++ AndroidApp/Core/ReminderItem.cs
@@ -1,16 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 
 namespace AndroidApp.Core
 {
     public class ReminderItem
     {
         public string Id { get; set; }
-        public DateTime Date { get; set; }
 
+        public DateTime Date { get; set; }
         public string Text { get; set; }
     }
 }
