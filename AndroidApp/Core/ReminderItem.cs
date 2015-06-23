1f868084ea341700a7920f391aa20495d6299758
Lucas Wyland <lwyland@swankmp.com>
2015-06-23 09:06:29 -0500

    Cleanup Of Core Code

--- AndroidApp/Core/ReminderItem.cs
+++ AndroidApp/Core/ReminderItem.cs
@@ -9,6 +9,7 @@ namespace AndroidApp.Core
     public class ReminderItem
     {
         public string Id { get; set; }
+
         public DateTime Date { get; set; }
 
         public string Text { get; set; }
