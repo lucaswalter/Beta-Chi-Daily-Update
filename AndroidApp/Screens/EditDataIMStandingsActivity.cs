b68b35962bc4aa6cad9f3855f9cd2e9605f9062a
Lucas <lucaswyland11@gmail.com>
2015-06-28 23:17:27 -0500

    Added Base Code For Retrieval Of Team Standings

--- AndroidApp/Screens/EditDataIMStandingsActivity.cs
+++ AndroidApp/Screens/EditDataIMStandingsActivity.cs
@@ -1,26 +1,100 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+using System.Threading.Tasks;
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
+using AndroidApp.Adapters;
+using AndroidApp.Core;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace AndroidApp.Screens
 {
-    [Activity()]
+    [Activity(Theme = "@style/Theme.BetaChi")]
     public class EditDataIMStandingsActivity : Activity
     {
-        protected override void OnCreate(Bundle bundle)
+
+        // Mobile Service Client Reference
+        private MobileServiceClient client;
+
+        // Mobile Service Tables Used To Access Data
+        private IMobileServiceTable<TeamItem> teamTable;
+
+        // Adapter To Sync Teams With The List
+        private TeamAdapter teamAdapter;
+
+        // Create Team List View
+        private ListView teamListView;
+
+        // Add Team Button
+        private Button addTeamButton;
+
+        protected override async void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.EditDataIMStandingsActivity);
 
-            // Create your application here
+            // Create Adapter To Bind The Reminder Items To The View
+            teamAdapter = new TeamAdapter(this);
+            teamListView = FindViewById<ListView>(Resource.Id.listViewEditIMStandings);
+            teamListView.Adapter = teamAdapter;
+
+            // Connect To Azure Mobile Service
+            try
+            {
+                // Initialize
+                CurrentPlatform.Init();
+
+                // Create Mobile Service Client Instance
+                client = new MobileServiceClient(Constants.APPLICATION_URL, Constants.APPLICATION_KEY);
+
+                // Retrieve Tables
+                teamTable = client.GetTable<TeamItem>();
+
+                // Load Data From The Mobile Service
+                await RefreshTeamsFromTableAsync();
+
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e, "Connection Error");
+            }   
+        }
+
+        // Retrieve Standing Data
+        async Task RefreshTeamsFromTableAsync()
+        {
+            try
+            {
+                // Get Today's Reminders
+                var list = await teamTable.OrderBy(x => x.Points).ToListAsync();
+
+                // Clear Reminder Adapter
+                teamAdapter.Clear();
+
+                // Add Reminders
+                foreach (TeamItem current in list)
+                    teamAdapter.Add(current);
+
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e, "Connection Error");
+            }
+        }
+
+        /** Error Dialog **/
+        void CreateAndShowDialog(Exception exception, String title)
+        {
+            CreateAndShowDialog(exception.Message, title);
+        }
+
+        void CreateAndShowDialog(string message, string title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
         }
     }
 }
