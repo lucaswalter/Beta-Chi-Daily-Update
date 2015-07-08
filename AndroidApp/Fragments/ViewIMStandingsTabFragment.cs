b67dd60defe438be259ac856fa79d6297bd9ced1
Lucas Wyland <lwyland@swankmp.com>
2015-07-08 17:39:01 -0500

    Added Layout Code For ViewIMStandingsTab

--- AndroidApp/Fragments/ViewIMStandingsTabFragment.cs
+++ AndroidApp/Fragments/ViewIMStandingsTabFragment.cs
@@ -1,11 +1,17 @@
 using Android.App;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
+using AndroidApp.Adapters;
 
 namespace AndroidApp.Resources.layout
 {
     public class ViewIMStandingsTabFragment : Fragment
     {
+
+        private ListView imStandingsImListView;
+        private TeamAdapter teamAdapter;
+
         public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -13,7 +19,8 @@ namespace AndroidApp.Resources.layout
             // Create Fragment View
             var view = inflater.Inflate(Resource.Layout.ViewIMStandingsTabFragment, container, false);
 
-            // TODO: Set Everything
+            
+            // Initialize Adapter And List View
 
             return view;
         }
