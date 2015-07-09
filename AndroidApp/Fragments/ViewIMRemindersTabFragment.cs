86447ac0813c26638d4b905acf95ed29cb1bbc1d
Lucas Wyland <lwyland@swankmp.com>
2015-07-09 08:57:08 -0500

    Added Layout Properties To IM Reminders Tab

--- AndroidApp/Fragments/ViewIMRemindersTabFragment.cs
+++ AndroidApp/Fragments/ViewIMRemindersTabFragment.cs
@@ -1,11 +1,17 @@
 using Android.App;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
+using AndroidApp.Adapters;
 
 namespace AndroidApp.Resources.layout
 {
     public class ViewIMRemindersTabFragment : Fragment
     {
+
+        private ListView ReminderListView;
+        private ReminderAdapter reminderAdapter;
+
         public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
